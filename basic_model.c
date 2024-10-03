ec2e8c2a4a7af1387ad31761ff82ca7096a6d4bb
Morgan Edwards <morganjedwards01@gmail.com>
2024-10-03 14:41:42 +0100

    changed course for the better

--- basic_model.c
+++ basic_model.c
@@ -9,80 +9,60 @@ typedef struct LeptonParams
     int64_t *prev_n;
     int64_t *next_n;
     double *gamma;
-
-    double (**terms)(double, double);
-
-    // physical parameters
-    double t_esc;
-    double t_dec;
-    double t_acc;
-    double S; // sync vs invers compton
+    double *delta_gamma;
 } LeptonParams;
 
 typedef struct SimulationParams
 {
     int64_t array_len;
-    double delta_gamma;
-    double delta_n;
+
     double t;
     double end_t;
     double dt;
-    double free_escape_time;
-    double background_rho;
-    double L;
+    
+    double tau_esc; // free escape time
+    double S;   // sync vs invers compton
+    double C;   // power law constant
+    double power;   // power law power
+    double gamma_m; // turning point energy
 
     int32_t n_species;
     LeptonParams **Species;
 
 } SimulationParams;
 
-double power_law_generator(double uniform_deviate, double start, double finish, double power)
+void malloc_Sim_Species_arrays(SimulationParams *Sim)
 {
-    return pow(((pow(finish, (power + 1)) - pow(start, (power + 1))) * uniform_deviate + pow(start, (power + 1))), (1 / (power + 1)));
-}
-
-double fill_distribution_power_law(int64_t *array, SimulationParams *Sim, double start, double finish, double power)
-{
-    double uniform_deviate;
-    int64_t index, i = 0; 
-
-    memset(array, 0 , Sim->array_len * sizeof(int64_t));
+    Sim->Species = malloc(Sim->n_species * sizeof(LeptonParams *));
     
-    while (i < Sim->array_len)
+    for (int32_t i = 0; i < Sim->n_species; i++)
     {
-        uniform_deviate = (double)rand() / (double)RAND_MAX;
-        index = (int64_t)power_law_generator(uniform_deviate, start, finish, power);
-        if (Sim->array_len > index)
-        {
-            array[index]++;
-            i++;
-        }
+        Sim->Species[i] = malloc(sizeof(LeptonParams));
+        Sim->Species[i]->next_n = calloc(Sim->array_len + 1, sizeof(int64_t));
+        Sim->Species[i]->prev_n = calloc(Sim->array_len + 1, sizeof(int64_t));
+        Sim->Species[i]->gamma = calloc(Sim->array_len, sizeof(double));
+
     }
-
 }
 
-double Q(double gamma)
+double I(double gamma, SimulationParams *Sim)
 {
-}
-
-double A_prime(double gamma, SimulationParams *Sim, LeptonParams *Lepton)
-{
-    return Sim->L - 1. / Lepton->t_esc - 1. / Lepton->t_dec - 1. / Lepton->t_acc - 2. * Lepton->S * gamma;
-}
-
-double B_prime(double gamma, LeptonParams *Lepton)
-{
-    return -gamma / Lepton->t_acc - Lepton->S * gamma * gamma;
+    if (gamma < Sim->gamma_m)
+    {
+        return 0.;
+    }
+    else
+    {
+        return Sim->C * gamma * exp(-1. * Sim->power);
+    }
 }
 
 void iteration(SimulationParams *Sim, LeptonParams *Lepton)
 {
-    for (int64_t i = Sim->gamma_zones - 1; i == 0; i--)
+    for (int64_t i = Sim->array_len; i == 0; i--)
     {
-        Lepton->next_n[i] = pow(exp(1.),
-                                (log(Sim->delta_gamma) / B_prime(Lepton->gamma[i], Lepton)) *
-                                        ((1. / Lepton->prev_n[i]) + A_prime(Lepton->gamma[i], Sim, Lepton)) + // removed injection term Q for now
-                                    log(Lepton->next_n[i + 1]));
+        Lepton->next_n[i] = (1. / (2. * Sim->S * Lepton->gamma[i] + (1. / Sim->tau_esc) - ((Sim->S * Lepton->gamma[i] * Lepton->gamma[i]) / Lepton->delta_gamma[i]))) *
+        (I(Lepton->gamma[i], Sim) - ((Sim->S * Lepton->gamma[i] * Lepton->gamma[i]) / Lepton->delta_gamma[i]) * Lepton->next_n[i+1]);
 
         // copy new value to previous array
         Lepton->prev_n[i] = Lepton->next_n[i];
@@ -91,6 +71,14 @@ void iteration(SimulationParams *Sim, LeptonParams *Lepton)
 
 void set_initial_state(SimulationParams *Sim)
 {
+    
+    for (int32_t i = 0; i < Sim->n_species; i++)
+    {
+        for (int64_t j = 0; j < Sim->array_len; j++)
+        {
+            Sim->Species[i]->next_n[Sim->array_len] = 0;
+        }
+    }
 }
 
 void simulate(SimulationParams *Sim)
@@ -104,4 +92,9 @@ void simulate(SimulationParams *Sim)
             iteration(Sim, Sim->Species[i]);
         }
     }
+}
+
+int main()
+{
+
 }
